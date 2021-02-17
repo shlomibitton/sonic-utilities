9fc9e07b47034d342b82066c88bd337644bd1ad0
Shlomi Bitton <shlomibi@nvidia.com>
2021-02-17 14:40:24 +0000

    address comments

--- tests/fast_reboot_dump_test.py
+++ tests/fast_reboot_dump_test.py
@@ -1,5 +1,6 @@
 import json
 import os
+from deepdiff import DeepDiff
 from utilities_common.db import Db
 import importlib
 fast_reboot_dump = importlib.import_module("scripts.fast-reboot-dump")
@@ -10,13 +11,13 @@ class TestFastRebootDump(object):
     def setup_class(cls):
         print("SETUP")
 
-        target = os.getcwd() + '/fast_reboot_dump_dbs'
+        test_db_dumps_directory = os.getcwd() + '/fast_reboot_dump_dbs'
         asic_db_object = Db()
         app_db_object = Db()
         asic_db = asic_db_object.db
         app_db = app_db_object.db
-        populate_db(asic_db, target, 'ASIC_DB.json')
-        populate_db(app_db, target, 'APP_DB.json')
+        populate_db(asic_db, test_db_dumps_directory, 'ASIC_DB.json')
+        populate_db(app_db, test_db_dumps_directory, 'APPL_DB.json')
 
         cls.asic_db = asic_db
         cls.app_db = app_db
@@ -25,33 +26,24 @@ class TestFastRebootDump(object):
         vlan_ifaces = ['Vlan2']
 
         fdb_entries, all_available_macs, map_mac_ip_per_vlan = fast_reboot_dump.generate_fdb_entries_logic(self.asic_db, self.app_db, vlan_ifaces)
-        expectd_fdb_entries = """\
-[{'FDB_TABLE:Vlan2:52-54-00-5D-FC-B7': {'type': 'dynamic', 'port': 'PortChannel0001'}, 'OP': 'SET'}]\
-"""
-        assert str(fdb_entries) == expectd_fdb_entries
 
-        expectd_all_available_macs = """\
-{('Vlan2', '52:54:00:5d:fc:b7')}\
-"""
-        assert str(all_available_macs) == expectd_all_available_macs
+        expectd_fdb_entries = [{'FDB_TABLE:Vlan2:52-54-00-5D-FC-B7': {'type': 'dynamic', 'port': 'PortChannel0001'}, 'OP': 'SET'}]
+        assert not DeepDiff(fdb_entries, expectd_fdb_entries, ignore_order=True)
 
-        expectd_map_mac_ip_per_vlan = """\
-{'Vlan2': {'52:54:00:5d:fc:b7': 'PortChannel0001'}}\
-"""
-        assert str(map_mac_ip_per_vlan) == expectd_map_mac_ip_per_vlan
+        expectd_all_available_macs = {('Vlan2', '52:54:00:5d:fc:b7')}
+        assert not DeepDiff(all_available_macs, expectd_all_available_macs, ignore_order=True)
+
+        expectd_map_mac_ip_per_vlan = {'Vlan2': {'52:54:00:5d:fc:b7': 'PortChannel0001'}}
+        assert not DeepDiff(map_mac_ip_per_vlan, expectd_map_mac_ip_per_vlan, ignore_order=True)
     
     @classmethod
     def teardown_class(cls):
         print("TEARDOWN")
 
-def populate_db(dbconn, target, db_name):
-    if (db_name == 'ASIC_DB.json'):
-        db = dbconn.ASIC_DB
-    elif (db_name == 'APP_DB.json'):
-        db = dbconn.APPL_DB
-    with open(target + '/' + db_name) as DB:
+def populate_db(dbconn, test_db_dumps_directory, db_dump_filename):
+    db = getattr(dbconn, db_dump_filename.replace('.json',''))
+    with open(test_db_dumps_directory + '/' + db_dump_filename) as DB:
         db_dump = json.load(DB)
-        DB.close()
-        for (table, fields) in db_dump.items():
-            for (key, value) in fields['value'].items():
+        for table, fields in db_dump.items():
+            for key, value in fields['value'].items():
                 dbconn.set(db, table, key, value)
